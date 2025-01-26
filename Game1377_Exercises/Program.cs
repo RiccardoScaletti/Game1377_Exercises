c0431f4b2e5afdcb9279109dfed6176f242df82e
Riccardo Scaletti <rickgamedeveloper@gmail.com>
2025-01-25 20:16:01 -0600

    Add project files.

---
+++ Game1377_Exercises/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Game1377_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
